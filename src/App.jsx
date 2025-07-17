0f01cb71555016b6cff70357447a92469955fc36
smraffick <rafficksm2k@gmail.com>
2025-07-17 10:34:36 +0200

    default css deleted and body color added

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Navigationbar from './components/Navigationbar'
 import Footer from './components/Footer'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Router> {/*gives routing capabilities to your app.*/}
       <Navigationbar />
-      <Router> {/*gives routing capabilities to your app.*/}
-        <Routes> {/*groups and selects which route to render.*/}
-          <Route path='/' element={<div>Home</div>} /> {/*defines how to map a URL path to a component.*/}
-          <Route path='about' element={<div>About</div>} />
-          <Route path='project' element={<h3>Project</h3>} />
-          <Route path='resume' element={<h3>Resume</h3>} />
-          <Route path='*' element={<Navigate to='/' />} />
-        </Routes>
-      </Router>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <Routes> {/*groups and selects which route to render.*/}
+        <Route path='/' element={<h3>Home</h3>} /> {/*defines how to map a URL path to a component.*/}
+        <Route path='about' element={<h3>About</h3>} />
+        <Route path='project' element={<h3>Project</h3>} />
+        <Route path='resume' element={<h3>Resume</h3>} />
+        <Route path='*' element={<Navigate to='/' />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
+    // <div className="card">
+    //   <button onClick={() => setCount((count) => count + 1)}>
+    //     count is {count}
+    //   </button>
+    // </div>
   )
 }
 
