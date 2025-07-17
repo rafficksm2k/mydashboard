425946863ca5db7c32c5ca0cbb610c101e2324d8
smraffick <rafficksm2k@gmail.com>
2025-07-17 10:05:41 +0200

    bootstrap added

--- src/components/Navigationbar.jsx
+++ src/components/Navigationbar.jsx
@@ -2,15 +2,22 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
+import logo from '../assets/react.svg'
 
 export default function Navigationbar() {
-    return (<Container>
-        <Nav>
-            <Nav.Link href='/'>Home</Nav.Link>
-            <Nav.Link href='/about'>About</Nav.Link>
-            <Nav.Link href='/project'>Project</Nav.Link>
-            <Nav.Link href='/resume'>Resume</Nav.Link>
-        </Nav>
-    </Container>
+    return (
+        <Navbar bg="dark" variant="dark" expand="md">
+            <Container>
+                <Navbar.Brand href="/" className="d-flex">
+                    <img src={logo} className="img-fluid logo" alt="brand" />
+                </Navbar.Brand>
+                <Nav>
+                    <Nav.Link href='/'>Home</Nav.Link>
+                    <Nav.Link href='/about'>About</Nav.Link>
+                    <Nav.Link href='/project'>Project</Nav.Link>
+                    <Nav.Link href='/resume'>Resume</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
     )
 }
