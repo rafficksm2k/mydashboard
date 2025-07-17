1cd4389415e73220741443a473b8dccd2029087b
smraffick <rafficksm2k@gmail.com>
2025-07-17 10:44:38 +0200

    css basic color added

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,18 @@ function App() {
 
   return (
     <Router> {/*gives routing capabilities to your app.*/}
-      <Navigationbar />
-      <Routes> {/*groups and selects which route to render.*/}
-        <Route path='/' element={<h3>Home</h3>} /> {/*defines how to map a URL path to a component.*/}
-        <Route path='about' element={<h3>About</h3>} />
-        <Route path='project' element={<h3>Project</h3>} />
-        <Route path='resume' element={<h3>Resume</h3>} />
-        <Route path='*' element={<Navigate to='/' />} />
-      </Routes>
-      <Footer />
-    </Router>
+      <div className="App">
+        <Navigationbar />
+        <Routes> {/*groups and selects which route to render.*/}
+          <Route path='/' element={<h3>Home</h3>} /> {/*defines how to map a URL path to a component.*/}
+          <Route path='about' element={<h3>About</h3>} />
+          <Route path='project' element={<h3>Project</h3>} />
+          <Route path='resume' element={<h3>Resume</h3>} />
+          <Route path='*' element={<Navigate to='/' />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router >
     // <div className="card">
     //   <button onClick={() => setCount((count) => count + 1)}>
     //     count is {count}
