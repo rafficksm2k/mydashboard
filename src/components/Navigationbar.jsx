e310e5bb25665096d33dfae06fe3c3c2d5eb4dd1
smraffick <rafficksm2k@gmail.com>
2025-07-18 10:16:49 +0200

    Page FLicker issue fixed - by using Nav.Link as='' to=''

--- src/components/Navigationbar.jsx
+++ src/components/Navigationbar.jsx
@@ -3,21 +3,30 @@ import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
 import logo from '../assets/react.svg'
+import { Link } from 'react-router-dom';
 
 export default function Navigationbar() {
     return (
-        <Navbar bg="dark" variant="dark" expand="md">
+        <Navbar bg="dark" variant='dark' expand="lg">
             <Container>
                 <Navbar.Brand href="/" className="d-flex">
                     <img src={logo} className="img-fluid logo" alt="brand" />
                 </Navbar.Brand>
-                <Nav>
-                    <Nav.Link href='/'>Home</Nav.Link>
-                    <Nav.Link href='/about'>About</Nav.Link>
-                    <Nav.Link href='/project'>Project</Nav.Link>
-                    <Nav.Link href='/resume'>Resume</Nav.Link>
-                </Nav>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About</Nav.Link>
+                        <Nav.Link as={Link} to='/project'>Project</Nav.Link>
+                        <Nav.Link as={Link} to='/resume'>Resume</Nav.Link>
+                        {/* <Nav.Link href='/project'>Project</Nav.Link>
+                        The above href makes the page flicker -
+                        it will reload the entire page!
+                        */}
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
+
     )
 }
