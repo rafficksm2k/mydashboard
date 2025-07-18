002980fd15938801e6c7e1f72062cf4d07c04770
smraffick <rafficksm2k@gmail.com>
2025-07-18 23:37:06 +0200

    React icons added for Navigation bar

--- src/components/Navigationbar.jsx
+++ src/components/Navigationbar.jsx
@@ -4,21 +4,37 @@ import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
 import logo from '../assets/react.svg'
 import { Link } from 'react-router-dom';
+import {
+    AiOutlineFileText,
+    AiOutlineHome,
+    AiOutlineFundProjectionScreen,
+    AiOutlineUser,
+} from "react-icons/ai";
+
+import '../styles/navbar.css'
 
 export default function Navigationbar() {
     return (
-        <Navbar bg="dark" variant='dark' expand="lg">
+        <Navbar bg="dark" variant='dark' expand="lg" className="navbar">
             <Container>
-                <Navbar.Brand href="/" className="d-flex">
+                <Navbar.Brand href='/' className="d-flex">
                     <img src={logo} className="img-fluid logo" alt="brand" />
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" /> {/*mobile view 3 dots*/}
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link as={Link} to="/">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/about">About</Nav.Link>
-                        <Nav.Link as={Link} to='/project'>Project</Nav.Link>
-                        <Nav.Link as={Link} to='/resume'>Resume</Nav.Link>
+                        <Nav.Link as={Link} to='/'>
+                            <AiOutlineHome style={{ marginBottom: "4px" }} />
+                            Home</Nav.Link>
+                        <Nav.Link as={Link} to='/about'>
+                            <AiOutlineUser style={{ marginBottom: "3px" }} />
+                            About</Nav.Link>
+                        <Nav.Link as={Link} to='/project'>
+                            <AiOutlineFundProjectionScreen style={{ marginBottom: "3px" }} />
+                            Projects</Nav.Link>
+                        <Nav.Link as={Link} to='/resume'>
+                            <AiOutlineFileText style={{ marginBottom: "3px" }} />
+                            Resume</Nav.Link>
                         {/* <Nav.Link href='/project'>Project</Nav.Link>
                         The above href makes the page flicker -
                         it will reload the entire page!
