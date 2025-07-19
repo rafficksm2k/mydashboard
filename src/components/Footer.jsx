1febf97345644d7d1b103dd1c7a398b22f12d438
smraffick <rafficksm2k@gmail.com>
2025-07-19 23:59:50 +0200

    Footer added - 3 color line and text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import '../styles/footer.scss'
 
 export default function Footer() {
     return (
-        <h3>Footer</h3>
+        <>
+            <Container className="container footer-wrapper">
+                <Row className="border">
+                    <Col xs="6" sm="6" md='6' className="first"></Col>
+                    <Col xs="4" sm="4" md='4' className="second"></Col>
+                    <Col xs="2" sm="2" md='2' className="third"></Col>
+                </Row>
+            </Container>
+            <Container className='mt-3'>
+                <Row>
+                    <Col md="6" className="footer-developedby">
+                        <p>Designed and Developed by Raffick</p>
+                    </Col>
+                    <Col md="6" className="footer-copywright">
+                        <p>Copyright © 2025</p>
+                    </Col>
+                </Row>
+            </Container>
+
+        </>
     )
 }
