f98626e7610e619c029bbdf6288020bc5c3a84fe
smraffick <rafficksm2k@gmail.com>
2025-07-21 13:53:16 +0200

    Home page image added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import home from '../assets/home.jpg'
 
 export default function Home() {
     return (
-        <div>
-            <h3>Home</h3>
-        </div>
+        <Container>
+            <Row>
+                <Col md={6}>
+                    <img src={home}
+                        alt="home pic"
+                        className="img-fluid"
+                        style={{ maxHeight: "450px" }}
+                    ></img>
+                </Col>
+                <Col md={6}><h3>Hi.. I am Raffick</h3>
+                    <div>Home content in progress...</div>
+                </Col>
+            </Row>
+        </Container>
     )
 }
