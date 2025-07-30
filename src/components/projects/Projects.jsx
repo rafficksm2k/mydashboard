ccc06050b40c5ed3ff5a1e23e6b1801641ee0ab4
smraffick <rafficksm2k@gmail.com>
2025-07-30 11:30:42 +0200

    added jnj, walgreens in projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -14,7 +14,7 @@ export default function Projects() {
                             <div class="flip-card-inner" style={{ transform: isFlipped ? 'rotateY(180deg)' : 'rotateY(0)' }}>
                                 <div class="flip-card-front">
                                     <img src='/metlife.jpg' className='metlife'></img>
-                                    <h4><strong className="main-name purple">MetLife Insurance Portal </strong></h4>
+                                    <h5><strong className="main-name purple">MetLife Insurance Portal </strong></h5>
                                     <p>React-based insurance platform for data visualization and policy management.</p>
                                 </div>
                                 <div class="flip-card-back">
@@ -37,13 +37,53 @@ export default function Projects() {
                             <div class="flip-card-inner" style={{ transform: isFlipped ? 'rotateY(180deg)' : 'rotateY(0)' }}>
                                 <div class="flip-card-front">
                                     <img src='/ssbt.jpg' className='metlife'></img>
-                                    <h4><strong className="main-name purple">State Street Bank – Financial Web Solutions</strong></h4>
+                                    <h5><strong className="main-name purple">State Street Bank – Financial Web Solutions</strong></h5>
                                     <p>React-based insurance platform for data visualization and policy management.</p>
                                 </div>
                                 <div class="flip-card-back">
                                     <ul>
                                         <li>Designed and developed secure web applications for State Street Bank (www.statestreet.com), ensuring compliance with banking and financial regulations.</li>
                                         <li>Focused on data protection, responsive UI, and performance optimization tailored for enterprise-scale financial workflows.</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col xs="12" sm="12" md='6' >
+                        <div class="flip-card" onClick={() => { setIsFlipped(!isFlipped) }}>
+                            <div class="flip-card-inner" style={{ transform: isFlipped ? 'rotateY(180deg)' : 'rotateY(0)' }}>
+                                <div class="flip-card-front">
+                                    <img src='/walgreens.jpg' className='metlife'></img>
+                                    <h5><strong className="main-name purple">Walgreens – E-Commerce Platform</strong></h5>
+                                    <p>Optimized high-traffic retail landing pages with React and JavaScript, enhancing performance and user experience.</p>
+                                </div>
+                                <div class="flip-card-back">
+                                    <ul>
+                                        <li>Developed and optimized high-traffic E-Commerce landing pages, improving load time by 80% for Walgreens (www.walgreens.com).</li>
+                                        <li>Built interactive store locator pages using Google Maps API for real-time geolocation and better UX.</li>
+                                        <li>Used JavaScript (ES6), React, Redux, and Java to build modular, scalable components.</li>
+                                        <li>Integrated RESTful APIs and enhanced cross-browser compatibility.</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+
+                    <Col xs="12" sm="12" md='6' >
+                        <div class="flip-card" onClick={() => { setIsFlipped(!isFlipped) }}>
+                            <div class="flip-card-inner" style={{ transform: isFlipped ? 'rotateY(180deg)' : 'rotateY(0)' }}>
+                                <div class="flip-card-front">
+                                    <img src='/jj.jpg' className='metlife'></img>
+                                    <h5><strong className="main-name purple"> Johnson & Johnson – Global Multisite Platform</strong></h5>
+                                    <p>Developed and maintained a Drupal-based, Java/GWT/PHP platform supporting 500+ high‑traffic brand sites internationally.</p>
+                                </div>
+                                <div class="flip-card-back">
+                                    <ul>
+                                        <li>Collaborated in building J&J’s centralized global digital experience platform (CANVAS), supporting over 500 brand and product websites using Drupal for content management .</li>
+                                        <li>Engineered secure, scalable backend solutions leveraging Java, PHP, and Google Web Toolkit (GWT) for interactive modules and business logic.</li>
                                     </ul>
                                 </div>
                             </div>
