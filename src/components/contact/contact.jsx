44e0d23a2c902ca3a5d957d173b90b26cf0b8533
smraffick <rafficksm2k@gmail.com>
2025-07-31 12:04:38 +0200

    input component changes

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Row, Col, Button, Form } from 'react-bootstrap'
 import '../../styles/contact.scss'
 
 export default function contact() {
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [message, setMessage] = useState();
+    const sendmail = () => {
+        console.log(name);
+        console.log(email);
+        console.log(message);
+        setName('');
+        setEmail('');
+        setMessage('');
+    }
     return (
         <>
             <Container >
@@ -12,17 +23,20 @@ export default function contact() {
                         <Form>
                             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                                 <Form.Label className='contact-text'>Your Name</Form.Label>
-                                <Form.Control type="email" placeholder="What's your good name?" />
+                                <Form.Control type="email" placeholder="What's your good name?"
+                                    value={name} onChange={(e) => { setName(e.target.value) }} />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                                 <Form.Label className='contact-text'>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="name@example.com" />
+                                <Form.Control type="email" placeholder="name@example.com"
+                                    value={email} onChange={(e) => { setEmail(e.target.value) }} />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                                 <Form.Label className='contact-text'>Your Message</Form.Label>
-                                <Form.Control as="textarea" rows={3} placeholder="What you want to say" />
+                                <Form.Control as="textarea" rows={3} placeholder="What you want to say"
+                                    value={message} onChange={(e) => { setMessage(e.target.value) }} />
                             </Form.Group>
-                            <Button className='send-button' variant="light">Send</Button>
+                            <Button className='send-button' variant="light" onClick={() => { sendmail() }}>Send</Button>
                         </Form>
                     </Col>
                     <Col className='contact-image' xs="12" sm="12" md='6'>
