23a14fe49db3eb1656feffa4b6f77bfdb52cd843
smraffick <rafficksm2k@gmail.com>
2025-08-04 11:00:24 +0200

    netfliy build

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,26 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { Container, Row, Col, Button, Form } from 'react-bootstrap'
+import emailjs from '@emailjs/browser';
 import '../../styles/contact.scss'
 
 export default function contact() {
     const [name, setName] = useState();
     const [email, setEmail] = useState();
     const [message, setMessage] = useState();
-    const sendmail = () => {
-        console.log(name);
-        console.log(email);
-        console.log(message);
-        setName('');
-        setEmail('');
-        setMessage('');
-    }
+    const form = useRef(); // This creates a reference
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        console.log('send email========');
+
+
+        emailjs.sendForm(
+            'service_r151vzf',     // Service ID from EmailJS
+            'template_jgsapdj',    // Template ID from EmailJS
+            form.current,
+            'NxBaz8diwrj18tRzD'      // Public Key from EmailJS
+        )
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
     return (
         <>
             <Container >
                 <Row>
                     <Col className='contact' xs="12" sm="12" md='6'>
                         <strong className="main-name purple contact-header"><h1>Contact</h1></strong>
-                        <Form>
+                        <Form ref={form}>
                             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                                 <Form.Label className='contact-text'>Your Name</Form.Label>
                                 <Form.Control type="email" placeholder="What's your good name?"
@@ -36,7 +49,7 @@ export default function contact() {
                                 <Form.Control as="textarea" rows={3} placeholder="What you want to say"
                                     value={message} onChange={(e) => { setMessage(e.target.value) }} />
                             </Form.Group>
-                            <Button className='send-button' variant="light" onClick={() => { sendmail() }}>Send</Button>
+                            <Button className='send-button' variant="light" onClick={sendEmail}>Send</Button>
                         </Form>
                     </Col>
                     <Col className='contact-image' xs="12" sm="12" md='6'>
