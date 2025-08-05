6e33365ea25911d5cd566f9ef7493bd9e339d82e
smraffick <rafficksm2k@gmail.com>
2025-08-05 11:31:20 +0200

    CRM 21 fix

--- src/components/Navigationbar.jsx
+++ src/components/Navigationbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
@@ -14,25 +14,27 @@ import { MdOutlineContactMail } from "react-icons/md";
 import '../styles/navbar.css'
 
 export default function Navigationbar() {
+    const [expand, setExpand] = useState(false);
     return (
-        <Navbar bg="dark" variant='dark' expand="lg" className="navbar fixednavbar">
+        <Navbar expanded={expand} bg="dark" variant='dark' expand="lg" className="navbar fixednavbar">
             <Container>
                 <Navbar.Brand href='/' className="d-flex">
                     <img src={logo} className="img-fluid logo" alt="brand" />
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" /> {/*mobile view 3 dots*/}
+                <Navbar.Toggle aria-controls="responsive-navbar-nav"
+                    onClick={() => setExpand(expand ? false : true)} /> {/*mobile view 3 dots*/}
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link as={Link} to='/'>
+                        <Nav.Link as={Link} to='/' onClick={() => { setExpand(false) }}>
                             <AiOutlineHome style={{ margin: "0px 3px 5px 0px" }} />
                             Home</Nav.Link>
-                        <Nav.Link as={Link} to='/project'>
+                        <Nav.Link as={Link} to='/project' onClick={() => setExpand(false)}>
                             <AiOutlineFundProjectionScreen style={{ margin: "0px 3px 4px 0px" }} />
                             Projects</Nav.Link>
-                        <Nav.Link as={Link} to='/resume'>
+                        <Nav.Link as={Link} to='/resume' onClick={() => setExpand(false)}>
                             <AiOutlineFileText style={{ margin: "0px 3px 4px 0px" }} />
                             Resume</Nav.Link>
-                        <Nav.Link as={Link} to='/contact'>
+                        <Nav.Link as={Link} to='/contact' onClick={() => setExpand(false)}>
                             <MdOutlineContactMail style={{ margin: "0px 3px 4px 0px" }} />
                             Contact</Nav.Link>
                     </Nav>
