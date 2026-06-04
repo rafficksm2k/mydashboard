6756a5fd1ac3819641a3ccb9c87d805d76fc5596
smraffick <rafficksm2k@gmail.com>
2026-06-05 00:09:09 +0200

    Update App.jsx
    Loading component removed for quick render

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,41 @@
-import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import Navigationbar from './components/Navigationbar'
-import Footer from './components/Footer'
-import Loader from './components/Loader'
-import Home from './components/home/Home'
-import Resume from './components/resume/Resume'
-import Contact from './components/contact/contact'
-import Projects from './components/projects/Projects'
-import './App.css'
+import { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Navigationbar from "./components/Navigationbar";
+import Footer from "./components/Footer";
+import Loader from "./components/Loader";
+import Home from "./components/home/Home";
+import Resume from "./components/resume/Resume";
+import Contact from "./components/contact/contact";
+import Projects from "./components/projects/Projects";
+import "./App.css";
 
 function App() {
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(
-    () => {
-      setTimeout(() => setLoading(false), 2000);
-    }, []
-  )
+  // useEffect(() => {
+  //   setTimeout(() => setLoading(false), 0);
+  // }, []);
 
   return (
-    <Router> {/*gives routing capabilities to your app.*/}
-      {loading ? <div className='loader-container'><Loader /> </div> :
-        < div className="App">
-          <Navigationbar />
-          <Routes> {/*groups and selects which route to render.*/}
-            <Route path='/' element={<Home />} /> {/*defines how to map a URL path to a component.*/}
-            <Route path='project' element={<Projects />} />
-            <Route path='resume' element={<Resume />} />
-            <Route path='contact' element={<Contact />} />
-            <Route path='*' element={<Navigate to='/' />} />
-          </Routes>
-          <Footer />
-        </div>}
-    </Router >
-  )
+    <Router>
+      <div className="App">
+        <Navigationbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="project" element={<Projects />} />
+          <Route path="resume" element={<Resume />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
