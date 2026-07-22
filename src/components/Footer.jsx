1b2021fe14df6e873da8008a73dcf17cb5667fca
smraffick <rafficksm2k@gmail.com>
2026-07-22 11:03:35 +0200

    LinkedIn added in footer and contact page
    LinkedIn added in footer and contact page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-import '../styles/footer.scss'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import RenderIcon from "./home/RenderIcon";
+import { IoLogoLinkedin } from "react-icons/io5";
+import "../styles/footer.scss";
 
 export default function Footer() {
-    return (
-        <>
-            <Container className="container footer-wrapper">
-                <Row className="border">
-                    <Col xs="12" sm="12" md='12' className="first"></Col>
-                </Row>
-            </Container>
-            <Container className='mt-3'>
-                <Row>
-                    <Col md="6" className="footer-developedby">
-                        <p>Designed and Developed by Raffick</p>
-                    </Col>
-                    <Col md="6" className="footer-copywright">
-                        <p>Copyright © 2025</p>
-                    </Col>
-                </Row>
-            </Container>
-
-        </>
-    )
+  return (
+    <>
+      <Container className="container footer-wrapper">
+        <Row className="border">
+          <Col xs="12" sm="12" md="12" className="first"></Col>
+        </Row>
+      </Container>
+      <Container className="mt-3">
+        <Row>
+          <Col md="4" className="footer-developedby">
+            <p>Designed and Developed by Raffick</p>
+          </Col>
+          <Col md="4" className="footer-copywright">
+            <p>Copyright © 2025</p>
+          </Col>
+          <Col md="4" className="footer-linkedin">
+            <a
+              href="https://www.linkedin.com/in/mohamed-raffick-seyan-031675291"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IoLogoLinkedin />
+            </a>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,163 +1,210 @@
-import React, { useState, useRef } from 'react'
-import { Container, Row, Col, Button, Form } from 'react-bootstrap'
-import emailjs from '@emailjs/browser';
-import '../../styles/contact.scss'
-import contactImage from '@/assets/contact.svg?w=600&format=webp'
-import ContactFormLabel from './ContactFormLabel'
-import ContactSuccessPage from './ContactSuccessPage'
+import React, { useState, useRef } from "react";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
+import emailjs from "@emailjs/browser";
+import { IoLogoLinkedin } from "react-icons/io5";
+import "../../styles/contact.scss";
+import contactImage from "@/assets/contact.svg?w=600&format=webp";
+import ContactFormLabel from "./ContactFormLabel";
+import ContactSuccessPage from "./ContactSuccessPage";
+import RenderIcon from "../home/RenderIcon";
+import linkedin from "@/assets/in.webp";
 
 export default function contact() {
+  const initValue = {
+    name: "",
+    email: "",
+    message: "",
+  };
 
-    const initValue = {
-        name: '',
-        email: '',
-        message: ''
-    }
+  const initValid = {
+    name: true,
+    email: true,
+    message: true,
+  };
 
-    const initValid = {
-        name: true,
-        email: true,
-        message: true
-    }
+  const [formValues, setFormValues] = useState(initValue);
+  const [formValid, setFormValid] = useState(initValid);
 
-    const [formValues, setFormValues] = useState(initValue)
-    const [formValid, setFormValid] = useState(initValid)
+  const [successPage, setSuccessPage] = useState(false);
+  const form = useRef(); // This creates a reference
 
+  const onInputChange = (value, type, validType) => {
+    setFormValues((prev) => ({
+      ...prev,
+      [type]: value,
+    }));
 
-    const [successPage, setSuccessPage] = useState(false);
-    const form = useRef(); // This creates a reference
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    const onInputChange = (value, type, validType) => {
-        setFormValues(prev => ({
-            ...prev, [type]: value
+    if (type === "email" && (!value || !emailRegex.test(value))) {
+      setFormValid((prev) => ({
+        ...prev,
+        [validType]: false,
+      }));
+    } else {
+      if (value == "" || value == null) {
+        setFormValid((prev) => ({
+          ...prev,
+          [validType]: false,
         }));
-
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-        if (type === 'email' && (!value || !emailRegex.test(value))) {
-            setFormValid(prev => ({
-                ...prev, [validType]: false
-            }));
-        }
-
-        else {
-            if (value == '' || value == null) {
-                setFormValid(prev => ({
-                    ...prev, [validType]: false
-                }));
-            }
-
-            else {
-                setFormValid(prev => ({
-                    ...prev, [validType]: true
-                }));
-            }
-        }
-
+      } else {
+        setFormValid((prev) => ({
+          ...prev,
+          [validType]: true,
+        }));
+      }
     }
+  };
 
-    const formDetails = [{
-        label: 'Your Name',
-        labelClassName: 'contact-text',
-        errorLabel: 'Please fill your name',
-        errorLabelClassName: 'contact-text red',
-        inputName: 'name',
-        inputType: 'name',
-        inputPlaceholder: 'Whats your good name?',
-        inputChange: onInputChange,
-        validType: 'name',
-        inputValue: formValues['name'],
-        isValid: formValid['name']
+  const formDetails = [
+    {
+      label: "Your Name",
+      labelClassName: "contact-text",
+      errorLabel: "Please fill your name",
+      errorLabelClassName: "contact-text red",
+      inputName: "name",
+      inputType: "name",
+      inputPlaceholder: "Whats your good name?",
+      inputChange: onInputChange,
+      validType: "name",
+      inputValue: formValues["name"],
+      isValid: formValid["name"],
     },
     {
-        label: 'Your Email',
-        labelClassName: 'contact-text',
-        errorLabel: 'Please fill your email',
-        errorLabelClassName: 'contact-text red',
-        inputName: 'email',
-        inputType: 'email',
-        inputPlaceholder: 'name@example.com',
-        inputChange: onInputChange,
-        inputValue: formValues['email'],
-        validType: 'email',
-        isValid: formValid['email']
+      label: "Your Email",
+      labelClassName: "contact-text",
+      errorLabel: "Please fill your email",
+      errorLabelClassName: "contact-text red",
+      inputName: "email",
+      inputType: "email",
+      inputPlaceholder: "name@example.com",
+      inputChange: onInputChange,
+      inputValue: formValues["email"],
+      validType: "email",
+      isValid: formValid["email"],
     },
     {
-        label: 'Your Message',
-        labelClassName: 'contact-text',
-        errorLabel: 'Please fill your message',
-        errorLabelClassName: 'contact-text red',
-        inputName: 'message',
-        inputType: 'message',
-        inputPlaceholder: 'What you want to say?',
-        inputChange: onInputChange,
-        inputValue: formValues['message'],
-        validType: 'message',
-        isValid: formValid['message']
-    }];
+      label: "Your Message",
+      labelClassName: "contact-text",
+      errorLabel: "Please fill your message",
+      errorLabelClassName: "contact-text red",
+      inputName: "message",
+      inputType: "message",
+      inputPlaceholder: "What you want to say?",
+      inputChange: onInputChange,
+      inputValue: formValues["message"],
+      validType: "message",
+      isValid: formValid["message"],
+    },
+  ];
 
-    const sendEmail = (e) => {
-        e.preventDefault();
-        const newValid = {};
-        Object.entries(formValues).forEach(([key, value]) => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+    const newValid = {};
+    Object.entries(formValues).forEach(([key, value]) => {
+      if (value == "" || value == null) {
+        newValid[key] = false;
+        setFormValid(newValid);
+      } else {
+        newValid[key] = true;
+        setFormValid(newValid);
+      }
+    });
 
-            if (value == '' || value == null) {
-                newValid[key] = false;
-                setFormValid(newValid);
-            }
-            else {
-                newValid[key] = true;
-                setFormValid(newValid);
-            }
-        });
+    if (Object.values(newValid).every((value) => value === true)) {
+      emailjs
+        .sendForm(
+          "service_r151vzf", // Service ID from EmailJS
+          "template_jgsapdj", // Template ID from EmailJS
+          form.current,
+          "NxBaz8diwrj18tRzD", // Public Key from EmailJS
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          },
+        );
+      setSuccessPage(true);
+    }
+  };
 
-        if (Object.values(newValid).every(value => value === true)) {
+  return (
+    <Container>
+      <Row>
+        <Col className="contact" xs="12" sm="12" md="6">
+          <strong className="main-name purple contact-header">
+            <h1>Contact Me</h1>
+          </strong>
+          {!successPage ? (
+            <Form ref={form}>
+              {formDetails.map((detail, index) => (
+                <Form.Group
+                  className="mb-3"
+                  key={index}
+                  controlId="exampleForm.ControlInput1"
+                >
+                  <ContactFormLabel
+                    text={detail.label}
+                    classNme={detail.labelClassName}
+                  />
+                  <Form.Control
+                    name={detail.inputName}
+                    type={detail.inputType}
+                    placeholder={detail.inputPlaceholder}
+                    value={detail.inputValue}
+                    onChange={(e) => {
+                      detail.inputChange(
+                        e.target.value,
+                        detail.inputName,
+                        detail.validType,
+                      );
+                    }}
+                  />
+                  {!detail.isValid ? (
+                    <ContactFormLabel
+                      text={detail.errorLabel}
+                      classNme={detail.errorLabelClassName}
+                    />
+                  ) : null}
+                </Form.Group>
+              ))}
 
-            emailjs.sendForm(
-                'service_r151vzf',     // Service ID from EmailJS
-                'template_jgsapdj',    // Template ID from EmailJS
-                form.current,
-                'NxBaz8diwrj18tRzD'      // Public Key from EmailJS
-            )
-                .then((result) => {
-                    console.log(result.text);
-                }, (error) => {
-                    console.log(error.text);
-                });
-            setSuccessPage(true);
-        }
-    };
-
-    return (
-        <Container >
-            <Row>
-                <Col className='contact' xs="12" sm="12" md='6'>
-                    <strong className="main-name purple contact-header"><h1>Contact Me</h1></strong>
-                    {
-                        !successPage ?
-                            <Form ref={form}>
-
-                                {formDetails.map((detail, index) =>
-                                (
-                                    <Form.Group className="mb-3" key={index} controlId="exampleForm.ControlInput1">
-                                        <ContactFormLabel text={detail.label} classNme={detail.labelClassName} />
-                                        <Form.Control name={detail.inputName} type={detail.inputType} placeholder={detail.inputPlaceholder}
-                                            value={detail.inputValue} onChange={(e) => { detail.inputChange(e.target.value, detail.inputName, detail.validType) }} />
-                                        {!detail.isValid ?
-                                            <ContactFormLabel text={detail.errorLabel} classNme={detail.errorLabelClassName} /> : null}
-                                    </Form.Group>
-                                ))
-                                }
-
-                                <Button className='send-button' variant="light" onClick={sendEmail}>Send</Button>
-                            </Form>
-                            : <ContactSuccessPage />
-                    }
-                </Col>
-                <Col className='contact-image' xs="12" sm="12" md='6'>
-                    <img src={contactImage}></img>
-                </Col>
-            </Row>
-        </Container>
-    )
+              <Button
+                className="send-button"
+                variant="light"
+                onClick={sendEmail}
+              >
+                Send
+              </Button>
+            </Form>
+          ) : (
+            <ContactSuccessPage />
+          )}
+        </Col>
+        <Col className="contact-image" xs="12" sm="12" md="6">
+          <img src={contactImage}></img>
+          <div className="linkedin-container">
+            <h2>{`Find Me On`}</h2>
+            <div className="col-md-12 d-flex justify-content-center">
+              <a
+                href="https://www.linkedin.com/in/mohamed-raffick-seyan-031675291"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="linkedin-img"
+                  src={linkedin}
+                  alt="linkedin"
+                ></img>
+                {/* <RenderIcon icon={IoLogoLinkedin} text="" /> */}
+              </a>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
